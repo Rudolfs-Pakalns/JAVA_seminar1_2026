12e83b5bc23e55f0f59b7ccbf086b13097461bdd
Rudolfs Pakalns <s23pakarudo@venta.lv>
2026-02-18 11:31:37 +0200

    Init; project for seminar 1 - java

---
+++ src/Seminars1/MainService.java
@@ -0,0 +1,159 @@
+package Seminars1;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.SortedMap;
+
+public class MainService 
+{
+
+	private static final float GRAVITY = 9.80665f;
+	
+	public static void main(String[] args) 
+	{
+		// --------------- 0. Uzdevums -------------------
+		String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt",
+				"Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron",
+		"Kate" };
+	
+		int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393,
+				299, 343, 317, 265 };
+	
+		int how_many_elements = names.length;
+		if(names.length > times.length)	how_many_elements = times.length;	
+		
+		for(int i = 0; i < how_many_elements; i++)
+			System.out.println(names[i] + " " + times[i] + "sec");
+
+		// --------------- 1. Uzdevums -------------------
+		try
+		{
+			System.out.println("Your position: " + positionCalc(-10, 5, 20) + "m");
+		}
+		
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}	
+		
+		// --------------- 2. Uzdevums -------------------
+		try
+		{
+			System.out.println("5! = " + factorialForLoop(5));
+		}
+
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}	
+
+		// --------------- 3. Uzdevums -------------------
+		try
+		{
+			double[] mas = generateArray(10, 3, 10);
+			System.out.println(Arrays.toString(mas));
+			System.out.println(getMean(mas));
+			System.out.println(arraySort(mas));
+		}
+		
+
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}	
+
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	// Ja main ir citas funkcijas, tās visticamāk būs static 
+	private static double positionCalc(double initialVelocity, double
+		initialPosition, double fallingTime) throws Exception
+	{
+		//x(t) = 0.5 × a*t^2 + v_0*t + x_0
+		
+		if(initialVelocity >= 0 && initialPosition >= 0 && fallingTime >= 0)
+		{
+			double result = 0.5 * GRAVITY * Math.pow(fallingTime, 2) + initialVelocity * fallingTime + initialPosition;
+			return result;
+		}
+		
+		else throw new Exception("Nevar aprekinat, jo kads no ievades datiem nav korekts");
+	}
+	
+	private static int factorialForLoop(int N) throws Exception
+	{
+		if(N > 0)
+		{
+			int res = 1;
+			for(int i = 1; i <= N; i++)
+				res *= i;
+			return res;
+		}
+		
+		else throw new Exception("Nevar aprekinat, jo ievades N ir negativs vai 0");
+	}
+	
+	private static double[] generateArray(int N, double lower, double upper) throws Exception
+	{
+		
+		if(N <= 0) throw new Exception("Nevar izveidot masivu, kura garums ir negativs");
+	
+		double[] result = new double[N];
+		Random rand = new Random();
+		
+		for(int i = 0; i < result.length; i++)
+			result[i] = rand.nextDouble(lower, upper);
+		
+		return result;
+	}
+	
+	private static double getMean(double[] array) throws Exception
+	{
+		if(array == null) throw new Exception("nevar aprekinat videjo vertibu, jo masiva nav elementu");
+		if(array.length == 0) throw new Exception("nevar aprekinat videjo vertibu, jo masiva nav elementu");
+		
+		double sum = 0;
+//		for(int i = 0; i < array.length; i++)
+//			sum += array[i];
+		
+		for(double tempA : array)
+		{
+			sum += tempA;
+		}
+		double meanValue = sum/array.length;
+		return meanValue;
+	}
+	
+//	private static double getMin(double[] array) throws Exception
+//	{
+//		
+//	}
+//	
+//	private static double getMax(double[] array) throws Exception
+//	{
+//		
+//	}
+//	
+	private static double[] arraySort(double[] array) throws Exception
+	{
+		if(array == null) throw new Exception("nevar kartot, jo masiva nav elementu");
+		if(array.length == 0) throw new Exception("nevar kartot, jo masiva nav elementu");
+		
+		for(int i = 0; i < array.length; i++)
+			for(int j = 0; j < array.length - 1; j++)
+				if(array[j] > array[j+1]) array[j] = array[j+1];
+			
+		
+		
+		return array;
+	}
+	
+	
+}
---
+++ src/Seminars1/Seminar_PART2.java
@@ -0,0 +1,14 @@
+package Seminars1;
+
+public class Seminar_PART2 {
+
+	public static void main(String[] args) {
+		double gravity = -9.81; // Earth's gravity in m/s^2
+		double initialVelocity = 0.0;
+		double fallingTime = 10.0;
+		double initialPosition = 0.0;
+		double finalPosition = 0.0;
+		System.out.println("Pos in "+fallingTime+"sec :"+finalPosition);
+	}
+
+}
